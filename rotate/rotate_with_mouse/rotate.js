81c1c8d3507cdd59f1ce882b16caf9a1ca7f542e
Junenatte-and-little-pig <sy5622_5@126.com>
2019-06-03 22:06:42 +0800

    for fun day 3, a little pity

--- rotate/rotate_with_mouse/rotate.js
+++ rotate/rotate_with_mouse/rotate.js
@@ -1,27 +1,18 @@
 $(document).mousemove(function (e) {
     var divPoint = {
-        Y: $("div").position().top + 200,
-        X: $("div").position().left + 200
+        Y: 300,
+        X: 675
     };
     var mousePoint = {
         Y: e.pageY,
         X: e.pageX
     };
-    var gapX = mousePoint.X - divPoint.X;
-    var gapY = mousePoint.Y - divPoint.Y;
-    if (gapX > 0) {
-        if (gapY > 0) {
-            gapX *= -1;
-        } else {
-            gapY *= -1;
-        }
-    } else {
-        gapY *= -1;
-    }
-    var maxX = $(window).width() - divPoint.X;
-    var maxY = $(window).height() - divPoint.Y;
+    var gapY = mousePoint.X - divPoint.X;
+    var gapX = divPoint.Y - mousePoint.Y;
+    var maxY = $(window).width() - divPoint.X;
+    var maxX = divPoint.Y - $(window).height();
     var angle = Math.sqrt(Math.pow(gapX, 2) + Math.pow(gapY, 2)) / Math.sqrt(Math.pow(maxX, 2) + Math.pow(maxY, 2));
-    angle *= 80;
+    angle *= 60;
     var rotate = "rotate3d(" + gapX + "," + gapY + ",0," + angle + "deg)";
     $("div").css("transform", rotate);
 });
